bd0e7795817369ffb56d470ab979c9889e99e32c
Tanish Pandey <tanishpandey14@gmail.com>
2024-07-21 20:39:09 +0530

    REQUEST VERIFICATION PAGE

---
+++ src/RequestVerification.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class RequestVerification extends Component {
+  render() {
+    return (
+      <div>RequestVerification</div>
+    )
+  }
+}
