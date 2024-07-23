ea864b4997e178356c5b8b2f8219232768ca9245
Tanish Pandey <tanishpandey14@gmail.com>
2024-07-23 19:09:24 +0530

    article details

--- src/Verification Flow/ArticleDetails.jsx
+++ src/Verification Flow/ArticleDetails.jsx
@@ -1,14 +1,59 @@
-import { Box } from '@mui/material'
-import React, { Component } from 'react'
+import React from 'react';
+import { Grid, Card, CardContent, CardMedia, Typography, Button, Box } from '@mui/material';
+import image from '../demo_image123.jpg'
 
-export default class ArticleDetails extends Component {
-  render() {
-    return (
-   <Box>
-    <Box></Box>
-    <Box></Box>
+const services = [
+  {
+    title: "METER READING SERVICE",
+    description: "Give tenants verifiable accounts of their utility usage with our meter reading services. Let us eliminate inaccurate, overestimated bills.",
+    imageUrl: "meter-reading-service.jpg",
+  },
+  {
+    title: "CUSTOMIZED TENANT INVOICES",
+    description: "We offer personalized invoicing for each client we serve. We also inform our customers of all leakages or other fixture issues.",
+    imageUrl: "customized-tenant-invoices.jpg",
+  },
+  {
+    title: "INSPECTION REPORTS",
+    description: "Gain itemized summaries of utility usage for all main and sub-meters on your property. We offer reports on a monthly or quarterly basis.",
+    imageUrl: "inspection-reports.jpg",
+  },
+  {
+    title: "CLIENT TESTIMONIALS",
+    description: "Find out what clients are saying about us! Our dedicated staff is proud to offer all of our clients prompt outstanding service.",
+    imageUrl: "client-testimonials.jpg",
+  },
+];
 
-   </Box>
-    )
-  }
-}
+const ArticleDetails = () => {
+  return (
+    <Grid container spacing={4} sx={{ padding: 4 }}>
+      {services.map((service, index) => (
+        <Grid item xs={12} sm={6} md={3} key={index}>
+          <Card sx={{ height: '100%' }}>
+          <Box
+              sx={{
+                height: 140,
+                backgroundImage: `url(${image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
+             <Typography>{service.title}</Typography> 
+            </Box>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {service.description}
+              </Typography>
+            </CardContent>
+            <Button size="small" sx={{ justifyContent: 'center', margin: 2 }}>
+              LEARN MORE
+            </Button>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default ArticleDetails;
