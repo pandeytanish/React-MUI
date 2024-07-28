6fd1c84daf89669318f08238dc74461402d734c6
Tanish Pandey <tanishpandey14@gmail.com>
2024-07-28 21:44:15 +0530

    creditors page

--- src/Verification Flow/creditors.jsx
+++ src/Verification Flow/creditors.jsx
@@ -3,7 +3,46 @@ import React, { Component } from 'react'
 export default class creditors extends Component {
   render() {
     return (
-      <div>creditors</div>
+      <Box sx={{  padding: '40px' }}>
+    <Grid container spacing={2}>
+      {articles.map((article, index) => (
+        <Grid  item xs={12} sm={6} md={4} lg={4} key={index}>
+           <Box sx={{ position: 'relative',height : '438px', width : '387px', borderRadius : '26px', border : 'none'}}>
+    <Box
+      sx={{ height :"280px",width :'387px',backgroundImage : `url(${BlogImage})`, backgroundPosition : 'center', backgroundSize : 'contain'}}
+    />
+    <Box sx={{  position: 'absolute',top: 8,left: 8,backgroundColor: '#DBEAFE',color: 'white',padding: '6px 12px 6px 12px',borderRadius: '4px',color : '#1D4ED8',fontSize : '14px',fontWeight : 400,borderRadius : '26px'}}>
+        Debt Collection Strategies
+        </Box>
+    <Box sx={{display : 'flex', flexDirection :'column', gap : '6px',marginTop : '12px'}}>
+      <Typography sx={{display : 'flex', gap : '8px', fontSize : '14px', fontWeight : 400, color : '#64748B'}}>
+        {article.date} • {article.readingTime}
+      </Typography>
+      <Typography sx={{fontSize : '18px', fontWeight : 600, color : '#101828'}}>
+        {article.title}
+      </Typography>
+      <Box sx={{  display: 'flex',alignItems: 'center',gap : '6px'}}>
+        <Avatar sx={{ height : '48px', width : '48px' , border : '1px solid black', borderRadius : '50px' }} src={image} /> 
+        <Box>
+          <Typography sx={{fontSize : '14px', fontWeight : 600, color : '#334155'}}>
+            {article.author}
+          </Typography>
+          <Typography sx={{fontSize : '14px', fontWeight : 400, color : '#64748B'}}>
+            {article.role}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  </Box>
+        </Grid>
+      ))}
+    </Grid>
+    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 6 }}>
+      <Button variant="contained" sx={{padding : '10px 16px 10px 16px',backgroundColor :'#1E3A8A',fontSize : '14px', fontWeight :'700',color :'#FFFFFF', textTransform : 'none'}}>
+        See more articles
+      </Button>
+    </Box>
+  </Box>
     )
   }
 }
