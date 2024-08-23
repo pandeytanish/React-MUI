7adfbd5c6ceff2261c5952b7f3776ebff2f6500d
Tanish Pandey <tanishpandey14@gmail.com>
2024-08-23 21:05:47 +0530

    mui component

--- src/adminDocuments.jsx
+++ src/adminDocuments.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class adminDocuments extends Component {
   render() {
     return (
-      <div>adminDocuments</div>
+      <Box>
+        
+      </Box>
     )
   }
 }
